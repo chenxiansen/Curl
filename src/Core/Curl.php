ee560554bf04da8fb9a2bdcf5012254d2c671d4d
chenxiansen <15951982354@163.com>
2020-06-05 15:17:05 +0800

    update

--- src/Core/Curl.php
+++ src/Core/Curl.php
@@ -11,51 +11,86 @@ namespace Curl\Core;
 
 class Curl
 {
-    use CurlTrait;
 
-    public $url = null;
-    public $config = array();
+    private $curl;
+    public  $url = null;
+    public  $config = array();
 
     public function __construct()
     {
-
+        if(isset($this->curl))
+        {
+            //请求前，先重置请求信息
+            $this->_reset();
+            return $this->curl;
+        }
+        $this->curl = curl_init();
     }
 
     public function get($url, $data = array())
     {
-        if (is_array($url)) {
-            $data = $url;
-            $url = (string)$this->url;
-        }
-        $this->setUrl($url, $data);
-        $this->setOpt(CURLOPT_CUSTOMREQUEST, 'GET');
-        $this->setOpt(CURLOPT_HTTPGET, true);
-        return $this->exec();
+        //设置请求参数
+        curl_setopt($this->curl, CURLOPT_URL, $url);
+        curl_setopt($this->curl, CURLOPT_HEADER, 0);
+        //打印请求信息
+        echo 'Curl Parameter: ' . json_encode($this->_getInfo());
+        //执行请求
+        $this->_exec();
     }
 
     public function post()
     {
-        // TODO: Implement post() method.
+
     }
 
-    public function head()
+    /**
+     * @return 返回执行结果
+     */
+    private function _exec()
     {
-        // TODO: Implement head() method.
+        return curl_exec($this->curl);
     }
 
-    public function delete()
+    /**
+     * @return 配置参数查看
+     */
+    public function _getInfo()
     {
-        // TODO: Implement delete() method.
+        return curl_getinfo($this->curl);
     }
 
-    public function options()
+    /**
+     * 报错信息
+     */
+    public function _error()
     {
-        // TODO: Implement options() method.
+        if(curl_errno($this->curl))
+        {
+            echo 'Curl error: ' . curl_error($this->curl);
+        }
     }
 
-    public function  put()
+    /**
+     * @return 版本信息
+     */
+    public function _version()
     {
-        // TODO: Implement put() method.
+        return curl_version();
     }
 
+    /**
+     * 重置
+     */
+    private function _reset()
+    {
+        return curl_reset($this->curl);
+    }
+
+    /**
+     * 释放资源
+     */
+    public function __destruct()
+    {
+        curl_close($this->curl);
+    }
 }
