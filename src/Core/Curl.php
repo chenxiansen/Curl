b4ae015675dd744870852041378362733dbfd873
chenxiansen <15951982354@163.com>
2020-06-11 11:14:45 +0800

    php>=7.2

--- src/Core/Curl.php
+++ src/Core/Curl.php
@@ -12,10 +12,12 @@ use Curl\Core\Contracts\Curl as CurlContract;
 
 class Curl implements CurlContract
 {
-
     private $curl;
-    public  $url = null;
-    public  $config = array();
+    private $config = [
+        "timeout"            =>    10,                   //超时时间
+        "header"             =>     0,                   //启用时会将头文件的信息作为数据流输出。 1：0 ，true：false
+        "return_transfer"    =>     1,                   //TRUE 将curl_exec()获取的信息以字符串返回，而不是直接输出。
+    ];
 
     public function __construct()
     {
@@ -23,47 +25,66 @@ class Curl implements CurlContract
         {
             $this->init();
         }
+        curl_reset($this->curl);
+        curl_setopt($this->curl, CURLOPT_TIMEOUT, $this->config["timeout"]);
+        curl_setopt($this->curl, CURLOPT_HEADER, $this->config["header"]);
+        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, $this->config["return_transfer"]);
+        curl_setopt($this->curl, CURLOPT_SSL_VERIFYPEER, false);    //绕过ssl验证
+        curl_setopt($this->curl, CURLOPT_SSL_VERIFYHOST, false);
     }
 
     public function get($uri)
     {
         //设置请求参数
         curl_setopt($this->curl, CURLOPT_URL, $uri);
-        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($this->curl, CURLOPT_HEADER, 0);
-        curl_setopt($this->curl, CURLOPT_SSL_VERIFYPEER, false);    //绕过ssl验证
-        curl_setopt($this->curl, CURLOPT_SSL_VERIFYHOST, false);
-        //打印请求信息
-        echo 'Curl Parameter: ' . json_encode($this->getInfo());
         //执行请求
         return $this->exec();
     }
 
     public function post($uri,$data = array())
     {
-
+        curl_setopt($this->curl, CURLOPT_URL, $uri);
+        curl_setopt($this->curl, CURLOPT_POST, 1);
+        curl_setopt($this->curl, CURLOPT_POSTFIELDS, $data);
+        return $this->exec();
     }
 
-
+    //初始化
     protected function init()
     {
         $this->curl = curl_init();
     }
 
+    //执行
     protected function exec()
     {
         //执行并获取内容
         $result = curl_exec($this->curl);
         //释放curl句柄
         curl_close($this->curl);
-        unset($this->curl);
 
         return $result;
     }
 
-
-    protected function getInfo()
+    //查看配置信息
+    public function getInfo($ch)
     {
-        return curl_getinfo($this->curl);
+        return curl_getinfo($ch ?? $this->curl);
     }
+
+    //设置参数
+    public function setConf($config = array())
+    {
+        foreach ($this->config as $conf)
+        {
+            $this->config[$conf] = $config[$conf];
+        }
+    }
+
+    //查看参数
+    public function getConf()
+    {
+        return $this->config;
+    }
+
 }
