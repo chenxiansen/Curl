865ad530adef601032a95edffa5403fb29c432e9
chenxiansen <15951982354@163.com>
2020-06-11 17:33:28 +0800

    error add

--- src/Core/Curl.php
+++ src/Core/Curl.php
@@ -14,14 +14,14 @@ class Curl implements CurlContract
 {
     private $curl;
     private $config = [
-        "CURLOPT_TIMEOUT"                   =>    10,                   //超时时间
-        "CURLOPT_HEADER"                    =>     0,                   //启用时会将头文件的信息作为数据流输出。 1：0 ，true：false
-        "CURLOPT_RETURNTRANSFER"            =>     1,                   //TRUE 将curl_exec()获取的信息以字符串返回，而不是直接输出。
-        "CURLOPT_SSL_VERIFYPEER"            =>     false,               //默认禁用https
-        "CURLOPT_SSL_VERIFYHOST"            =>     false,               //默认禁用https
-        "CURLOPT_PROXYAUTH"                 =>     null,                //代理认证模式
-        "CURLOPT_PROXY"                     =>     null,                //代理服务器ip
-        "CURLOPT_PROXYPORT"                 =>     null,                //代理服务器port
+        CURLOPT_TIMEOUT                   =>    10,                   //超时时间
+        CURLOPT_HEADER                    =>     0,                   //启用时会将头文件的信息作为数据流输出。 1：0 ，true：false
+        CURLOPT_RETURNTRANSFER            =>     1,                   //TRUE 将curl_exec()获取的信息以字符串返回，而不是直接输出。
+        CURLOPT_SSL_VERIFYPEER            =>     false,               //默认禁用https
+        CURLOPT_SSL_VERIFYHOST            =>     false,               //默认禁用https
+        CURLOPT_PROXYAUTH                 =>     null,                //代理认证模式    CURLAUTH_BASIC
+        CURLOPT_PROXY                     =>     null,                //代理服务器ip
+        CURLOPT_PROXYPORT                 =>     null,                //代理服务器port
     ];
 
     public function __construct()
@@ -35,34 +35,20 @@ class Curl implements CurlContract
 
     public function get($uri)
     {
-        //设置请求参数
-        curl_setopt($this->curl, CURLOPT_URL, $uri);
-        //执行请求
+        $this->setConf([
+            CURLOPT_URL           =>$uri
+        ]);
         return $this->exec();
     }
 
     public function post($uri,$data = [])
     {
-        curl_setopt($this->curl, CURLOPT_URL, $uri);
-        curl_setopt($this->curl, CURLOPT_POST, 1);
-        curl_setopt($this->curl, CURLOPT_POSTFIELDS, $data);
+        $this->setConf([
+            CURLOPT_URL           =>$uri,
+            CURLOPT_POST          =>1,
+            CURLOPT_POSTFIELDS    =>$data
+        ]);
         return $this->exec();
-    }
-
-    //初始化
-    protected function init()
-    {
-        $this->curl = curl_init();
-    }
-
-    //执行
-    protected function exec()
-    {
-        //执行并获取内容
-        $result = curl_exec($this->curl);
-        $this->close();
-
-        return $result;
     }
 
     //查看配置信息
@@ -84,6 +70,31 @@ class Curl implements CurlContract
         $this->resetConf($this->config);
     }
 
+    //查看参数
+    public function getConf()
+    {
+        return $this->config;
+    }
+
+    private function init()
+    {
+        $this->curl = curl_init();
+    }
+
+    private function exec()
+    {
+        //执行并获取内容
+        $result = curl_exec($this->curl);
+        //错误检测
+        if($this->error())
+        {
+            return $this->error();
+        }
+        $this->close();
+
+        return $result;
+    }
+
     //重置资源
     private function resetConf(array $config)
     {
@@ -98,15 +109,22 @@ class Curl implements CurlContract
         curl_setopt_array($this->curl,$config);
     }
 
-    //查看参数
-    public function getConf()
+    //释放资源
+    private function close()
     {
-        return $this->config;
+        curl_close($this->curl);
     }
 
-    //释放资源
-    protected function close($ch = null)
+    //错误检测
+    private function error()
     {
-        curl_close($ch ?? $this->curl);
+        if(curl_error($this->curl))
+        {
+            $error_msg = curl_error($this->curl);
+            $error_no  = curl_errno($this->curl);
+            $this->close();
+            return ["error_msg"=>$error_msg,"error_no"=>$error_no];
+        }
+        return false;
     }
 }
